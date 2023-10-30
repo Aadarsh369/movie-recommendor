82dd185975df1ee90017bd11e013614eb5f72cd5
Udayan <udayandas272013@gmail.com>
2023-10-30 18:00:39 +0530

    Updated components: Movie_highllight and Movie_List, Added files: data.js and index.js

---
+++ src/Resources/data.js
@@ -0,0 +1,19 @@
+let img_data = [
+    {
+        id: 1,
+        title: 'Green Lantern',
+        src: 'https://i.etsystatic.com/34853406/r/il/e731a3/4809681245/il_1588xN.4809681245_g8q2.jpg',
+        author: 'Netflix'
+    }, {
+        id: 4,
+        title: 'John wick',
+        src: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg',
+        author: 'Netflix',
+    }, {
+        id: 3,
+        title: 'Avengers',
+        src: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg',
+        author: 'Marvel Studios',
+    }
+];
+module.exports = img_data;
--- src/components/MovieHighlight.jsx
+++ src/components/MovieHighlight.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 function MovieHighlight() {
     return (
         <div className="movie-highlight">
-            <img src="path-to-highlight-image.jpg" alt="Highlight Movie" />
-            <h1>Movie-highlights</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque cumque officiis eos!</p>
-            <button>Watch now</button>
+           <button>Watch now</button>
         </div>
     );
 }
